cdf8199589839f1c98ddd58aca486c79fd74b04b
martin-linden <martin.linden@student.kyh.se>
2020-08-04 12:59:45 +0200

    error img fix

--- wines.js
+++ wines.js
@@ -12,7 +12,7 @@ const postData = [
         producer: "Fontaleoni",
         country: "Italien",
         number: "309434",
-        taste: ["../../taste/beef2.svg", "../../taste/bird.svg", "../../taste/lamb2.svg"],
+
         year: "2018",
         type: "../../type/red.svg",
         price: "599"
@@ -25,7 +25,7 @@ const postData = [
         name: "Barbera d´Asti La Gena",
         country: "Italien",
         number: "569433",
-        taste: ["../../taste/elk.svg", "../../taste/fish2.svg", "../../taste/drink2.svg", "../../taste/bird.svg"],
+
         year: "2015",
         type: "../../type/red.svg",
         price: "299"
@@ -38,7 +38,7 @@ const postData = [
         producer: "Fontaleoni",
         country: "Italien",
         number: "309434",
-        taste: ["../../taste/fish2.svg", "../../taste/seafood.svg", "../../taste/bird.svg", "../../taste/vegetable.svg"],
+
         year: "2018",
         type: "../../type/white.svg",
         price: "599"
@@ -51,7 +51,7 @@ const postData = [
         name: "Intrigante Frizzante",
         country: "Italien",
         number: "569433",
-        taste: ["../../taste/drink2.svg"],
+
         year: "2016",
         type: "../../type/rose.svg",
         price: "299"
