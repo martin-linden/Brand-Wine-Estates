eeea1fc6e551336f13e5bb6029702c63c0e12a9d
martin-linden <martin.linden@student.kyh.se>
2020-08-05 13:46:03 +0200

    image vissible with gatsby image nad works with flexbox for now

--- src/templates/singleWinePage.js
+++ src/templates/singleWinePage.js
@@ -12,6 +12,7 @@ query ($slug: String!) {
     slug
     country
     producer
+    year
     price
     image {
       childImageSharp {
@@ -32,27 +33,13 @@ const singleWinePage = ({ data }) => {
     return (
 
         <CardWrapper>
-            <div className="wine-bottle">
+            <div className="hej">
                 <Image
                     fluid={wine.image.childImageSharp.fluid}
                     alt={wine.title}
-                    id="wine-img" />
+                />
             </div>
-            <div className="info-section">
-                <h6>{wine.grape}</h6>
-                <h4>{wine.name}</h4>
-                <h5>{wine.producer} - {wine.country}</h5>
-                {/* <h5>{props.number}</h5> */}
-                <div className="goes-with">
-                    {/*  <img src={props.taste[0]} id="icon-img" />
-                    <img src={props.taste[1]} id="icon-img" />
-                    <img src={props.taste[2]} id="icon-img" />
-                    <img src={props.taste[3]} id="icon-img" /> */}
-                </div>
-                <div className="year-type">
-                    {/* <h5>{`${props.year}`} </h5>  <img src={props.type} id="type-img" /> */}
-                </div>
-            </div>
+
         </CardWrapper >
     )
 };
@@ -62,66 +49,19 @@ export default singleWinePage;
 
 
 const CardWrapper = styled.div`
-${ tw`shadow flex flex-col justify-start hover:shadow-lg cursor-pointer`}
-height: autopx;
-width: auto;
+${ tw`shadow flex flex-col justify-start items-center hover:shadow-lg cursor-pointer`}
+height: 400px;
+width: 200px;
 padding: 20px;
 background: #fff;
 border-radius: 5px;
 font-family: Assistant;
 
-.wine-bottle{
-    ${ tw`flex justify-center `}
-    
-}
-
-.info-section{
-    ${ tw`mt-5 `}
-}
-
-.year-type{
-    ${ tw`flex items-center`}
-}
-
-#wine-img{
-    ${ tw` m-0 `}
+.hej{
     width: 70px;
-    max-height: 210px;
-    min-height: 210px;
+    
+   
+   
 }
-#icon-img{
-    ${ tw` mr-2 mt-0 mb-1`}
-    width: 20px;
-    max-height: 20px;
-}
-
-#type-img{
-    ${ tw` mb-1 mt-0 ml-1`}
-    width: 25px;
-    max-height: 23px;
-}
-
-
-h4{
-    font-family: Assistant;
-    ${ tw` mb-1 `}
-}
-
-h3{
-    font-family: Assistant;
-    ${ tw` mb-1 `}
-}
-h5{
-    font-family: Assistant;
-    ${ tw` mb-1 `}
-}
-h6{
-    font-family: Assistant;
-    ${ tw` mb-1 `}
-}
-.goes-with{
-    ${ tw` mb-5 mt-5 flex items-center  `}
-}
-
 
 `
