4c34ebc3157d568540e4d1907552e34a49948d70
martin-linden <martin.linden@student.kyh.se>
2020-08-16 14:56:25 +0200

    added prismic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,4 @@
 module.exports = {
-	plugins: [
-		{
-			resolve: `gatsby-plugin-prefetch-google-fonts`,
-			options: {
-				fonts: [
-					{
-						family: `Poiret One`,
-						variants: [`400`]
-					},
-				],
-			},
-		}
-	],
 	siteMetadata: {
 		title: `Brand Wine Estates`,
 		description: `Wine Import`,
@@ -48,9 +35,14 @@ module.exports = {
 				icon: `src/images/wine.png` // This path is relative to the root of the site.
 			}
 		},
-		`gatsby-plugin-styled-components`
-		// this (optional) plugin enables Progressive Web App + Offline functionality
-		// To learn more, visit: https://gatsby.app/offline
-		// 'gatsby-plugin-offline',
+		`gatsby-plugin-styled-components`,
+		{
+			resolve: 'gatsby-source-prismic-graphql',
+			options: {
+				repositoryName: 'brandwineestates'
+			}
+		}
+
 	]
 };
+
